b2211778633226e1ff69531af0719e3f3418a341
Ercan ÇELEN <32388905+CelenErcan@users.noreply.github.com>
2018-10-18 00:05:30 +0300

    Remove, Top10, Between endPoint

--- routes/movie.js
+++ routes/movie.js
@@ -15,6 +15,32 @@ router.get('/', (req, res) => {
    });
 });
 
+//Top10 Listeleme, limit, sort
+router.get('/top10', (req, res) => {
+   const promise = Movie.find({ }).limit(10).sort({imdb_score: -1});
+   
+   promise.then((data) => {
+      res.json(data); 
+   }).catch((err) => {
+      res.json(err); 
+   });
+});
+
+//İki tarih arası arama gte(büyük yada eşit) gt(büyük), lte (küçük yada eşit) lt (küçük)
+router.get('/between/:start_year/:end_year', (req, res) => {
+   const {start_year, end_year} = req.params;
+   const promise = Movie.find({ 
+       year: {"gte": parseInt(start_year), "lte": parseInt(end_year)}
+   });
+   
+   promise.then((data) => {
+      res.json(data); 
+   }).catch((err) => {
+      res.json(err); 
+   });
+});
+
+
 //Id bazlı tek film getirir.
 router.get('/:movie_id', (req, res, next) => {
     const  promise = Movie.findById(req.params.movie_id);
@@ -70,7 +96,8 @@ router.post('/', (req, res, next) => {
      promise.then((data) => {
          res.json({status: 1});
      }).catch((err) => {
-         res.json(err);
+         res.json("hataaaaa");
+         console.log("hataaaaa");
      });
     
     
